3ec4892594d4d59c0fed047f7578a298525e6976
Ron Nelson <ronnelson929@gmail.com>
2025-03-09 15:28:23 -0500

    update the header theme

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -4,15 +4,15 @@
     $: activeUrl = $page.url.pathname;
   </script>
   
-  <Navbar class="w-full mx-auto mx-20 pt-10">
+  <Navbar class="w-full mx-auto px-20 pt-4 bg-blue-500 border-b-orange-500 border-b-4">
     <NavBrand href="/">
       <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
-      <span class="self-center text-blue-500 whitespace-nowrap text-4xl font-semibold">SyntaxSnark <br/>
-        <span class="self-center text-blue-400 whitespace-nowrap text-lg">A daily dose of IT Snark</span>
+      <span class="self-center text-white whitespace-nowrap text-4xl drop-shadow-xl font-semibold">SyntaxSnark <br/>
+        <span class="self-center text-orange-100 whitespace-nowrap text-lg">A daily dose of Snark</span>
     </span>
     </NavBrand>
-    <NavUl>
-      <NavLi href="/">Home</NavLi>
-      <NavLi href="/dashboard">Dashboard</NavLi>
+    <NavUl class="w-full mx-auto">
+      <NavLi class="text-xl text-orange-500" href="/">Home</NavLi>
+      <NavLi class="text-xl text-orange-500" href="/dashboard">Dashboard</NavLi>
     </NavUl>
   </Navbar>
