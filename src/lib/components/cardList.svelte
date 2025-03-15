338e06cc683fe0d38eeec5b48719f611fd4e45d1
Ron Nelson <ronnelson929@gmail.com>
2025-03-14 19:07:39 -0500

    updating columns

--- src/lib/components/cardList.svelte
+++ src/lib/components/cardList.svelte
@@ -6,7 +6,7 @@
     let {comments} = $props();
 </script>
 <div class="w-3/4 sm:w-full">
-    <div transition:fade class="xl:grid xl:grid-cols-6 xl:gap-4 sm:grid sm:grid-cols-1">
+    <div transition:fade class="xl:grid xl:grid-cols-4 xl:gap-4 sm:grid sm:grid-cols-1">
     {#each comments as comment }
         <Card class="pb-2" comment={comment} />
     {/each}
